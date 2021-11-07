0eda5c65be57b938f35f4221636702a43243ba25
Andre Silva <andre.silva@mercedes-benz.io>
2021-11-07 23:12:49 +0000

    using AFRAME lib to create components and set its properties

--- src/index.js
+++ src/index.js
@@ -2,36 +2,108 @@ require('aframe');
 require('aframe-event-set-component');
 require('aframe-look-at-component');
 
-AFRAME.registerComponent('start-configurations', {
+AFRAME.registerComponent('mb-btn', {
+    schema: {
+        name: {
+            type: 'string',
+            default: '',
+        },
+        position: {
+            type: 'string',
+            default: '0 0 0',
+        },
+        radius: {
+            type: 'number',
+            default: .15,
+        },
+        lookAt: {
+            type: 'string',
+            default: '#camera',
+        },
+        class: {
+            type: 'string',
+            default: 'clickable',
+        },
+        animation: {
+            type: 'string',
+            default: `property: scale; loop: true;
+                from: .9 .9 .9; to: 1.1 1.1 1.1;
+                dir: alternate; dur: 2000;`,
+        }
+    },
     init: function () {
-        document.getElementById('btn_details').addEventListener('click', () => {
-            document.getElementById('label_details').setAttribute('visible', true);
+        this.el.setAttribute('id', `btn_${this.data.name}`);
+        this.el.setAttribute('position', this.data.position);
+        this.el.setAttribute('radius', this.data.radius);
+        this.el.setAttribute('look-at', this.data.lookAt);
+        this.el.setAttribute('class', this.data.class);
+        this.el.setAttribute('animation__shining', this.data.animation);
+
+        this.el.addEventListener('mouseenter', () => {
+            document.getElementById('cursor').setAttribute('color', '#dc3545');
         });
-        
-        document.getElementById('btn_cabin').addEventListener('click', () => {
-            document.getElementById('label_cabin').setAttribute('visible', true);
+
+        this.el.addEventListener('mouseleave', () => {
+            document.getElementById('cursor').setAttribute('color', '#fff');
         });
-        
-        document.getElementById('btn_seat').addEventListener('click', () => {
-            document.getElementById('label_seat').setAttribute('visible', true);
+
+        document.getElementById(`btn_${this.data.name}`).addEventListener('click', () => {
+            document.getElementById(`label_${this.data.name}`).setAttribute('visible', true);
         });
-            
-        document.getElementById('btn_details').addEventListener('mouseleave', () => {
+
+        document.getElementById(`btn_${this.data.name}`).addEventListener('mouseleave', () => {
             setTimeout(() => {
-            document.getElementById('label_details').setAttribute('visible', false);
+                document.getElementById(`label_${this.data.name}`).setAttribute('visible', false);
             }, 3000);
         });
-        
-        document.getElementById('btn_cabin').addEventListener('mouseleave', () => {
-            setTimeout(() => {
-            document.getElementById('label_cabin').setAttribute('visible', false);
-            }, 3000);
-        });
-        
-        document.getElementById('btn_seat').addEventListener('mouseleave', () => {
-            setTimeout(() => {
-            document.getElementById('label_seat').setAttribute('visible', false);
-            }, 3000);
-        });
+    }
+});
+
+AFRAME.registerComponent('mb-label-group', {
+    schema: {
+        name: {
+            type: 'string',
+            default: '',
+        },
+        position: {
+            type: 'string',
+            default: '0 0 0',
+        },
+        visible: {
+            type: 'boolean',
+            default: false,
+        },
+    },
+    init: function() {
+        this.el.setAttribute('id', `label_${this.data.name}`);
+        this.el.setAttribute('position', this.data.position);
+        this.el.setAttribute('visible', this.data.visible);
+    }
+});
+
+AFRAME.registerComponent('mb-label', {
+    schema: {
+        value: {
+            type: 'string',
+            default: '',
+        },
+        lookAt: {
+            type: 'string',
+            default: '#camera',
+        },
+        position: {
+            type: 'string',
+            default: '0 0 0',
+        },
+        align: {
+            type: 'string',
+            default: 'left',
+        },
+    },
+    init: function() {
+        this.el.setAttribute('value', this.data.value);
+        this.el.setAttribute('position', this.data.position);
+        this.el.setAttribute('look-at', this.data.lookAt);
+        this.el.setAttribute('align', this.data.align);
     }
 });
